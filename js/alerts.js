0cef02b6a1a66292d57a2824359058b35bc41913
matheus victor <matheusvictorgdr15@gmail.com>
2022-06-19 17:21:15 -0300

    Login e home completos

---
+++ js/alerts.js
@@ -0,0 +1,16 @@
+document.getElementById("enter").onclick = function () {
+    Swal.fire({
+        icon: 'success',
+        title: 'Login correct',
+        showConfirmButton: false,
+        timer: 2000
+    })
+};
+
+// setTimeout(document.getElementById("enter").onclick = function() {
+//     window.location.href = "../pages/home.html";
+// }, 2000);
+
+
+
+
